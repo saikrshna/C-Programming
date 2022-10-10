dc3c54788fc8b3db196ee1141ed672b036e78295
saikrshna <thinkwithsai@gmail.com>
2022-10-10 21:55:59 +0530

    first commit

---
+++ question4.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<math.h>
+ int main()
+ {
+     int option;
+     float l,b,a,d, c, s, result1, result2;
+     printf("Enter 1 for AREA OF RECTANGLE\nEnter 2 for AREA OF TRIANGLE:");
+     scanf("%d", &option);
+     
+    if(option==1){
+    printf("Enter Length and breadth of rectangle:");
+    scanf("%f%f", &l, &b);
+
+    result1=l*b;
+    printf("The Area Of Rectangle is %f:", result1);
+
+    }else if(option==2){
+    printf("Enter 3 sides of Triangle:");
+    scanf("%f%f%f", &a, &d, &c );
+
+    s=(a+d+c)/2;
+    result2= sqrt(s*(s-a)*(s-d)*(s-c));
+    printf("The Area of Triangle is %f", result2);
+}else{
+ printf("!!!!!Invalid Insertion!!!!!");
+}
+    return 0;
+ }
